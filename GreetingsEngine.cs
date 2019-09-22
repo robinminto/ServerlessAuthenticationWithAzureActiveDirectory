95ba926b12f90fd72b0ba61f304ce2bbcdc79696
Robin Minto <robin@minto.co.uk>
2019-09-22 21:06:59 +0100

    New function to get claims

--- GreetingsEngine.cs
+++ GreetingsEngine.cs
@@ -1,5 +1,7 @@
-using System;
 using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -29,6 +31,23 @@ namespace ServerlessAuthenticationWithAzureActiveDirectory
                 ? (ActionResult)new OkObjectResult(new GreetingResponse
                     { Greeting = $"Hello, {name}", AuthenticationType = "Azure Active Directory" })
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+        [FunctionName("GetClaims")]
+        public static async Task<IActionResult> GetClaims(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+            HttpRequest request,
+            ILogger log)
+        {
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                var claimsPrincipal = (ClaimsPrincipal)Thread.CurrentPrincipal;
+                var claims = claimsPrincipal.Claims.ToDictionary(c => c.Type, c => c.Value);
+                // Could use the claims here. For this sample, just return it!
+                return new OkObjectResult(claims);
+            }
+
+            return new UnauthorizedResult();
         }
     }
 }
